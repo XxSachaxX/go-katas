c6fd4ab1255d6a6e16d8a18f4a2296a5ca61fd6f
XxSachaxX <sacha@gdpr.dev>
2025-11-05 13:51:48 +0100

    Handle basic birthday message creation for a given friend

--- birthday_greetings/get_friends_data.go
+++ birthday_greetings/get_friends_data.go
@@ -2,6 +2,7 @@ package birthday_greetings
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 )
 
@@ -17,10 +18,33 @@ func BuildFriends(data string) ([]Friend, error) {
 		return []Friend{}, errors.New("data is empty")
 	}
 
+	friendsData := strings.Split(data, "\n")
 	friends := []Friend{}
-	parts := strings.Split(data, ",")
-	lastName, firstName, birthDate, email := parts[0], parts[1], parts[2], parts[3]
-	friends = append(friends, Friend{LastName: lastName, FirstName: firstName, BirthDate: birthDate, Email: email})
+	for _, friend := range friendsData {
+		parts := strings.Split(friend, ",")
+		lastName, firstName, birthDate, email := parts[0], parts[1], parts[2], parts[3]
+		friends = append(friends, Friend{LastName: lastName, FirstName: firstName, BirthDate: birthDate, Email: email})
+	}
 
 	return friends, nil
+}
+
+func (friend Friend) BuildBirthdayMessage() (string, error) {
+	if friend.BirthDate == "" {
+		return "", errors.New("birth date is empty")
+	}
+
+	if friend.Email == "" {
+		return "", errors.New("email is empty")
+	}
+
+	if friend.FirstName == "" {
+		return "", errors.New("first name is empty")
+	}
+
+	if friend.LastName == "" {
+		return "", errors.New("last name is empty")
+	}
+
+	return fmt.Sprintf("Happy birthday, dear %s %s!", friend.FirstName, friend.LastName), nil
 }
--- birthday_greetings/get_friends_data_test.go
+++ birthday_greetings/get_friends_data_test.go
@@ -41,3 +41,101 @@ func TestBuildFriendsWithDifferentSingleFriendData(t *testing.T) {
 		t.Errorf("Expected %v but got %v", want, got)
 	}
 }
+
+func TestBuildFriendsWithMultipleFriendsData(t *testing.T) {
+	data := "Smith,Jane,1990/05/15,jane.smith@example.com\nDoe,John,1982/10/08,john.doe@example.com"
+	got, err := BuildFriends(data)
+	want := []Friend{
+		{FirstName: "Jane", LastName: "Smith", BirthDate: "1990/05/15", Email: "jane.smith@example.com"},
+		{FirstName: "John", LastName: "Doe", BirthDate: "1982/10/08", Email: "john.doe@example.com"},
+	}
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Expected %v but got %v", want, got)
+	}
+}
+
+func TestBuildBirthdayMessageWithDataLessFriend(t *testing.T) {
+	friend := Friend{FirstName: "", LastName: "", BirthDate: "", Email: ""}
+
+	_, err := friend.BuildBirthdayMessage()
+
+	if err == nil {
+		t.Errorf("Expected error to be raised but was not")
+	}
+}
+
+func TestBuildBirthdayMessageWithoutFriendFirstName(t *testing.T) {
+	friend := Friend{FirstName: "", LastName: "Smith", BirthDate: "1990/05/15", Email: "jane.smith@example.com"}
+
+	_, err := friend.BuildBirthdayMessage()
+
+	if err == nil {
+		t.Errorf("Expected error to be raised but was not")
+	}
+
+	if err.Error() != "first name is empty" {
+		t.Errorf("Expected error message to be 'friend's first name is empty' but got '%v'", err.Error())
+	}
+}
+
+func TestBuildBirthdayMessageWithoutFriendLastName(t *testing.T) {
+	friend := Friend{FirstName: "Jane", LastName: "", BirthDate: "1990/05/15", Email: "jane.smith@example.com"}
+
+	_, err := friend.BuildBirthdayMessage()
+
+	if err == nil {
+		t.Errorf("Expected error to be raised but was not")
+	}
+
+	if err.Error() != "last name is empty" {
+		t.Errorf("Expected error message to be 'friend's first name is empty' but got '%v'", err.Error())
+	}
+}
+
+func TestBuildBirthdayMessageWithoutFriendBirthdate(t *testing.T) {
+	friend := Friend{FirstName: "Jane", LastName: "Doe", BirthDate: "", Email: "jane.smith@example.com"}
+
+	_, err := friend.BuildBirthdayMessage()
+
+	if err == nil {
+		t.Errorf("Expected error to be raised but was not")
+	}
+
+	if err.Error() != "birth date is empty" {
+		t.Errorf("Expected error message to be 'friend's first name is empty' but got '%v'", err.Error())
+	}
+}
+
+func TestBuildBirthdayMessageWithoutFriendEmail(t *testing.T) {
+	friend := Friend{FirstName: "Jane", LastName: "Doe", BirthDate: "1990/05/15", Email: ""}
+
+	_, err := friend.BuildBirthdayMessage()
+
+	if err == nil {
+		t.Errorf("Expected error to be raised but was not")
+	}
+
+	if err.Error() != "email is empty" {
+		t.Errorf("Expected error message to be 'friend's first name is empty' but got '%v'", err.Error())
+	}
+}
+
+func TestBuildBirthdayMessageWithAFriendWithAllData(t *testing.T) {
+	friend := Friend{FirstName: "Jane", LastName: "Doe", BirthDate: "1990/05/15", Email: "jane.smith@example.com"}
+
+	got, err := friend.BuildBirthdayMessage()
+	want := "Happy birthday, dear Jane Doe!"
+
+	if err != nil {
+		t.Errorf("Expected no error but got '%v'", err.Error())
+	}
+
+	if got != want {
+		t.Errorf("Expected '%v' but got '%v'", want, got)
+	}
+}
